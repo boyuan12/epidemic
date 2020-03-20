bee6e98d31d7441a6985873b74aee2635f5fd13c
boyuan12 <boyuanliu6@yahoo.com>
2020-03-20 16:48:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'epidemic',         # How you named your package folder (MyLib)
   packages = ['epidemic'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is a python library that can help predict when will next epidemic happen (relatively). MAY NOT BE ACCURATE',   # Give a short description about your library
   long_description=long_description,
